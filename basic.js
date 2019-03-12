8900d8900c988f40f23174b7561d5e415062691d
Julien Coquet <julien.coquet@gmail.com>
2019-03-12 09:55:09 +0100

    Update basic.js

--- basic.js
+++ basic.js
@@ -5,15 +5,15 @@ Run this in console mode in your browser after switching to the custom reports l
 function deleteCustomReports(){
     var _reps = document.querySelectorAll(".ACTION-view");
     for (var i = 0; i < _reps.length; i++){
-        var el = reps[0];
+        var el = _reps[i];
         var elc = el.attributes["class"].textContent;
         var chunks = elc.split(" ");
         chunks[1] = "ACTION-delete";
         elc = chunks.join(" ");
-		_reps[i].className = elc;
+	_reps[i].className = elc;
     }
 }
 deleteCustomReports();
 /*
-Now click each click and save considerable time
+Now click each link and save considerable time
 */
